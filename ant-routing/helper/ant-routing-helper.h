d03005bbca1ae9c9190224fd05f752c6e2c2d554
Martijn <martijn.sauwens@gmail.com>
2019-05-01 15:48:44 +0200

    Init module

---
+++ ant-routing/helper/ant-routing-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef ANT_ROUTING_HELPER_H
+#define ANT_ROUTING_HELPER_H
+
+#include "ns3/ant-routing.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* ANT_ROUTING_HELPER_H */
+
---
+++ ant-routing/model/ant-routing.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef ANT_ROUTING_H
+#define ANT_ROUTING_H
+
+#include "ns3/core-module.h"
+
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* ANT_ROUTING_H */

4913921229defa07b62bda5b5638435a2e053a32
Bo Kleynen <kleynenbo@gmail.com>
2019-05-04 21:30:17 +0200

    Implement AntHeader

--- ant-routing/model/ant-packet.h
+++ ant-routing/model/ant-packet.h
@@ -17,6 +17,7 @@ enum AntType
   ProactiveForwardAnt = 1,
   ProactiveBroadcastAnt = 2,
   BackwardAnt = 3,
+  RouteRepairAnt = 4;
 };
 
 /**
@@ -42,21 +43,32 @@ enum AntType
 class AntHeader : public Header
 {
 public:
-   static TypeId GetTypeId ();
-   uint32_t GetSerializedSize () const;
-   void Serialize (Buffer::Iterator start) const;
-   uint32_t Deserialize (Buffer::Iterator start);
-   void Print (std::ostream &os) const;
+  AntHeader (AntType antType = AntType.ReactiveForwardAnt, uint8_t hopCount = 0,
+            uint8_t broadcastCount = 0, uint8_t backwardCount = 0,
+            uint32_t m_generation = 0, Ipv4Address dst = Ipv4Address (),
+            Ipv4Address m_origin = Ipv4Address (), Time timeEstimate = Time ());
+
+  AntHeader (AntType m_antType = AntType.ReactiveForwardAnt,uint8_t m_hopCount = 0,
+            uint8_t m_broadcastCount = 0, uint8_t m_backwardCount = 0,
+            uint32_t m_generation = 0, Ipv4Address dst = Ipv4Address (),
+            Ipv4Address m_origin = Ipv4Address (), Time timeEstimate = Time (),
+            std::vector<Ipv4Address> visitedNodes);
+
+  static TypeId GetTypeId ();
+  uint32_t GetSerializedSize () const;
+  void Serialize (Buffer::Iterator start) const;
+  uint32_t Deserialize (Buffer::Iterator start);
+  void Print (std::ostream &os) const;
 private:
-  AntType                   antType;
-  uint8_t                   hopCount;
-  uint8_t                   broadcastCount;
-  uint8_t                   backwardCount;
-  uint32_t                  generation;
-  Ipv4Address               origin;
-  Ipv4Address               dst;
-  Time                      timeEstimate;
-  std::vector<Ipv4Address>  visitedNodes;
+  AntType                   m_antType;
+  uint8_t                   m_hopCount;
+  uint8_t                   m_broadcastCount;
+  uint8_t                   m_backwardCount;
+  uint32_t                  m_generation;
+  Ipv4Address               m_origin;
+  Ipv4Address               m_dst;
+  Time                      m_timeEstimate;
+  std::vector<Ipv4Address>  m_visitedNodes;
 } // class ReactiveForwardAnt
 } // namespace ant_routing
 } // namespace

3b01c65cfd9e9a34ed5406680c6fc42e8042828d
Bo <kleynenbo@gmail.com>
2019-05-15 02:40:49 +0200

    Remove Vagrant stuff

--- provision.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get upgrade
-apt-get install -y vim gcc g++ python mercurial python-setuptools git qt5-default autoconf cvs bzr unrar gdb valgrind gsl-bin libgslcblas0 libgsl-dev flex bison libfl-dev tcpdump sqlite sqlite3 libsqlite3-dev libxml2 libxml2-dev doxygen graphviz imagemagick texlive texlive-extra-utils texlive-latex-extra texlive-font-utils texlive-lang-portuguese dvipng latexmk python-sphinx dia
